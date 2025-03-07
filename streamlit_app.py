7ce8e24ea390bbc594ffacaec844cd9743b72f56
Prudhvi Reddy <99823583+prudhvi-reddy-m@users.noreply.github.com>
2025-03-06 22:09:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,12 @@ class VaR:
         
     def data(self):
         df = yf.download(self.ticker, self.start, self.end)
-        self.adj_close_df = df["Adj Close"]
+    
+        if len(self.ticker) == 1:
+            self.adj_close_df = df[['Adj Close']]  # Ensure DataFrame structure even for single ticker
+        else:
+            self.adj_close_df = df['Adj Close']  # Multi-ticker case, pulls whole Adj Close table
+    
         self.log_returns_df = np.log(self.adj_close_df / self.adj_close_df.shift(1))
         self.log_returns_df = self.log_returns_df.dropna()
         self.equal_weights = np.array([1 / len(self.ticker)] * len(self.ticker))
